ee1cb9f6707626102b9cde9209c87bc9b699e6f6
G1Singh <gurnaaz.singh@gmail.com>
2022-10-19 17:42:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,26 +3,22 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-     res.send("<h1>Express Demo app here</h1> <h4>Success</h4>");
+  res.send('<h1>Express Demo App</h1> <h4>Message: Success</h4> <p>Version 1.1</p>');
 })
 
-
-app.get('/hard', (req, res) => {
-     res.send([
-          {
-               productId: '101', 
-               name: 'fuck'
-          },
-          {
-               productId: '102'
-          },
-          {
-               productId: '103'
-          },
-     ]);
+app.get('/products', (req, res) => {
+  res.send([
+    {
+      productId: '101',
+      price: 100
+    },
+    {
+      productId: '102',
+      price: 150
+    }
+  ])
 })
 
-
-app.listen(port, () => {
-     console.log('Demo app is up and listening to port: ', port);
+app.listen(port, ()=> {
+  console.log(`Demo app is up and listening to port: ${port}`);
 })
